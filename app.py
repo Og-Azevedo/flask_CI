4c84bfd2bb11fb413d2a31bd2d080dcd34834572
Og-Azevedo <ogazevedo.py@gmail.com>
2022-07-07 08:50:49 -0300

    env

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET = os.getenv("SECRET")
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Integração contínua</h1>"
+    return f"<h1>Integração contínua</h1> <p>{SECRET}</p>"
 
 if __name__=="__main__":
     app.run(debug=True)
