1308153bdbf7c907fc4ccdc3c3135c8dd517edad
Og Azevedo <ogazevedo.py@gmail.com>
2022-07-09 13:50:07 -0300

    pyodbc dockerfile

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
-import pyodbc
+# import pyodbc
 
 
 load_dotenv()
@@ -16,31 +16,31 @@ app = Flask(__name__)
 def hello_world():
     return f"<h1>Integração contínua</h1> <p>{SECRET}</p>"
 
-@app.route("/banco")
-def banco():
-    try:
-        dados_conexao = (
-            "Driver={SQL Server};"
-            f"Server={server};"
-            f"Database={database};"
-        )
-
-        connection = pyodbc.connect(dados_conexao)
-        print('connected')
-
-        cursor = connection.cursor()
-        cursor.execute(f"SELECT top 100 * FROM {tabela}")
-
-    except:
-        print('Erro na conexão como banco!')
-
-    html_string = ""
-
-    if cursor.rowcount:
-        for row in cursor.fetchall():
-            html_string = html_string + f"<p>{row}</p>"
-
-    return html_string
+# @app.route("/banco")
+# def banco():
+#     try:
+#         dados_conexao = (
+#             "Driver={SQL Server};"
+#             f"Server={server};"
+#             f"Database={database};"
+#         )
+#
+#         connection = pyodbc.connect(dados_conexao)
+#         print('connected')
+#
+#         cursor = connection.cursor()
+#         cursor.execute(f"SELECT top 100 * FROM {tabela}")
+#
+#     except:
+#         print('Erro na conexão como banco!')
+#
+#     html_string = ""
+#
+#     if cursor.rowcount:
+#         for row in cursor.fetchall():
+#             html_string = html_string + f"<p>{row}</p>"
+#
+#     return html_string
 
 if __name__=="__main__":
 
